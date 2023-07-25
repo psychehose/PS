8c644e09d120cfe67d64f3337496d1285a3f2583
psychehose <psychehose@gmail.com>
2023-07-25 20:41:28 +0900

    decode string c#

---
+++ LeetCode/Decode_String/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Decode_String;
+
+Solution solution = new Solution();
+
+var r1= solution.DecodeString("100[leetcode]");
+var r2 = solution.DecodeString("53[aba]");
+var r3 = solution.DecodeString("3[a]2[bc]");
+
+
+Console.WriteLine(r1);
+Console.WriteLine(r2);
+Console.WriteLine(r3);
+
